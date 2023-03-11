1701a0b94cebb9b8d0d319088e3982ffc2153d96
Claudiu-Gabriel STAICU (119436) <claudiu.staicu@stud.acs.upb.ro>
2023-03-11 14:41:09 +0200

    added flask

--- server/init.py
+++ server/init.py
@@ -1,45 +1,59 @@
 import os
 import replicate
+from flask import Flask, request, jsonify
 
-os.environ['REPLICATE_API_TOKEN'] = '40c55e97d7f9d9ecc8e16b00d1195109b07393a3'
-api_token = os.environ.get('REPLICATE_API_TOKEN')
+app = Flask(__name__)
 
-client = replicate.Client(api_token=api_token)
 
-model = replicate.models.get("stability-ai/stable-diffusion")
-version = model.versions.get(
-    "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf")
+@app.route('/api/submit', methods=['POST'])
+def submit():
+    input_data = request.json.get('inputData')
+    output_data = textToImage(input_data)
+    # result = {'status': 'success'}
+    return jsonify(output_data)
 
-# https://replicate.com/stability-ai/stable-diffusion/versions/db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf#input
-inputs = {
-    # Input prompt
-    'prompt': "italian breakfast",
 
-    # pixel dimensions of output image
-    'image_dimensions': "768x768",
+def textToImage(text):
+    os.environ['REPLICATE_API_TOKEN'] = '40c55e97d7f9d9ecc8e16b00d1195109b07393a3'
+    api_token = os.environ.get('REPLICATE_API_TOKEN')
 
-    # Specify things to not see in the output
-    # 'negative_prompt': ...,
+    client = replicate.Client(api_token=api_token)
 
-    # Number of images to output.
-    # Range: 1 to 4
-    'num_outputs': 1,
+    model = replicate.models.get("stability-ai/stable-diffusion")
+    version = model.versions.get(
+        "db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf")
 
-    # Number of denoising steps
-    # Range: 1 to 500
-    'num_inference_steps': 50,
+    # https://replicate.com/stability-ai/stable-diffusion/versions/db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf#input
+    inputs = {
+        # Input prompt
+        'prompt': text,
 
-    # Scale for classifier-free guidance
-    # Range: 1 to 20
-    'guidance_scale': 7.5,
+        # pixel dimensions of output image
+        'image_dimensions': "768x768",
 
-    # Choose a scheduler.
-    'scheduler': "DPMSolverMultistep",
+        # Specify things to not see in the output
+        # 'negative_prompt': ...,
 
-    # Random seed. Leave blank to randomize the seed
-    # 'seed': ...,
-}
+        # Number of images to output.
+        # Range: 1 to 4
+        'num_outputs': 1,
 
-# https://replicate.com/stability-ai/stable-diffusion/versions/db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf#output-schema
-output = version.predict(**inputs)
-print(output)
+        # Number of denoising steps
+        # Range: 1 to 500
+        'num_inference_steps': 50,
+
+        # Scale for classifier-free guidance
+        # Range: 1 to 20
+        'guidance_scale': 7.5,
+
+        # Choose a scheduler.
+        'scheduler': "DPMSolverMultistep",
+
+        # Random seed. Leave blank to randomize the seed
+        # 'seed': ...,
+    }
+
+    # https://replicate.com/stability-ai/stable-diffusion/versions/db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf#output-schema
+    output = version.predict(**inputs)
+    print(output)
+    return output
