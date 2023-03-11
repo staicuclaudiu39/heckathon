dbe88bcd35fc8726dd6c81d040e4f96e977bef02
mermaidsknees <ecaterina.febian@gmail.com>
2023-03-11 19:49:09 +0200

    Add button

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -25,11 +25,63 @@ const Image = styled("img")`
   height: 400px;
 `;
 
-const Center = styled('div')`
+const Center = styled("div")`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
+
+const ButtonWrapper = styled(Center)`
+  margin-top: 15px;
+`;
+
+const Button = styled("button")`
+  align-items: center;
+  appearance: none;
+  background-color: #fcfcfd;
+  border-radius: 4px;
+  border-width: 0;
+  box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,
+    rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #d6d6e7 0 -3px 0 inset;
+  box-sizing: border-box;
+  color: #36395a;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: "JetBrains Mono", monospace;
+  height: 48px;
+  justify-content: center;
+  line-height: 1;
+  list-style: none;
+  overflow: hidden;
+  padding-left: 16px;
+  padding-right: 16px;
+  position: relative;
+  text-align: left;
+  text-decoration: none;
+  transition: box-shadow 0.15s, transform 0.15s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  will-change: box-shadow, transform;
+  font-size: 18px;
+
+  &:focus {
+    box-shadow: #d6d6e7 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px,
+      rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #d6d6e7 0 -3px 0 inset;
+  }
+
+  &:hover {
+    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px,
+      rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #d6d6e7 0 -3px 0 inset;
+    transform: translateY(-2px);
+  }
+
+  &:active {
+    box-shadow: #d6d6e7 0 3px 7px inset;
+    transform: translateY(2px);
+  }
+`;
 
 const Searchbar = () => {
   const [search, setSearch] = useState("");
@@ -57,6 +109,8 @@ const Searchbar = () => {
     setImage(content);
   };
 
+  const handleCreateNFT = () => {};
+
   if (loading) {
     return (
       <Center>
@@ -75,6 +129,9 @@ const Searchbar = () => {
         key={image}
       >
         <Image src={image} />
+        <ButtonWrapper>
+          <Button onClick={handleCreateNFT}>Create NFT</Button>
+        </ButtonWrapper>
       </motion.div>
     </AnimatePresence>
   ) : (
